9945b793aff2f701222723252c29969b09bd3d18
Adam Gao <g6164567@gmail.com>
2026-03-25 17:26:33 +0100

    feat: add basic health check api endpoint

---
+++ src/main/java/com/munkamaci/demo/HealthController.java
@@ -0,0 +1,12 @@
+package com.munkamaci.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/api/health")
+    public String check() {
+        return "Munkamaci Backend is Live";
+    }
+}
