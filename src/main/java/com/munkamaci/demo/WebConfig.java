cf55f5a4801b6d794155e974f3c39871dbf6a0de
Adam Gao <g6164567@gmail.com>
2026-03-27 16:49:18 +0100

    fix: add new link for CORS setting

--- src/main/java/com/munkamaci/demo/WebConfig.java
+++ src/main/java/com/munkamaci/demo/WebConfig.java
@@ -16,7 +16,8 @@ public class WebConfig {
                 registry.addMapping("/**")
                         .allowedOrigins(
                                 "http://localhost:5173",
-                                "https://munkamaci.hu"
+                                "https://munkamaci.hu",
+                                "https://munka-maci-frontend.vercel.app/"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
